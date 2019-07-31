2fedb41486320bcd35e945f24a8081867d1cb8c6
liupei <liupei@didiglobal.com>
2019-07-31 21:01:15 +0800

    fixed(Amount): fixed Precision when animate percent  =1

--- components/amount/test/index.spec.js
+++ components/amount/test/index.spec.js
@@ -1,5 +1,5 @@
 import {Amount} from 'mand-mobile'
-import {shallowMount} from '@vue/test-utils'
+import {shallowMount, mount} from '@vue/test-utils'
 
 describe('Amount - Operation', () => {
   let wrapper
@@ -16,5 +16,43 @@ describe('Amount - Operation', () => {
       },
     })
     expect(wrapper.vm.isMounted).toBe(true)
+  })
+
+  test('Transition - 01', async () => {
+    wrapper = shallowMount({
+      template: `
+          <md-amount
+          :value="val"
+          :duration="800"
+          transition
+          ref="mdAmount"
+        ></md-amount>
+      `,
+      components: {
+        [Amount.name]: Amount,
+      },
+      data() {
+        return {
+          val: 1000,
+        }
+      },
+    })
+    expect(wrapper.vm.val).toBe(1000)
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$refs.mdAmount.formatValue).toBe(1000)
+    })
+
+    await wrapper.vm.$nextTick(() => {
+      wrapper.setData({
+        val: 20.66,
+      })
+    })
+
+    expect(wrapper.vm.val).toBe(20.66)
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$refs.mdAmount).toBe(20.66)
+    })
   })
 })
